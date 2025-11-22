4353b04ff5b5d864d90364c962e728a781590e1d
bittokks@outlook.com <bittokks@outlook.com>
2025-11-22 12:28:56 +0300

    Added SQL migration revert

--- migrations/20251119134718_init.down.sql
+++ migrations/20251119134718_init.down.sql
@@ -1,2 +1,9 @@
 -- Add down migration script here
 
+-- Drop Indices
+DROP INDEX IF EXISTS idx_users_email;
+DROP INDEX IF EXISTS idx_oauth_account_user_id;
+
+-- Drop Tables
+DROP TABLE IF EXISTS oauth_accounts;
+DROP TABLE IF EXISTS users;
